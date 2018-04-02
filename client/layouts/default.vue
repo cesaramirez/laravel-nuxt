13bed38d73aedd89230be4991e509a1015ee267b
César A. Ramírez <cesar.ramirez.sv@gmail.com>
2018-04-01 23:07:04 -0600

    :sparkles: Add nuxt client

---
+++ client/layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <top-nav></top-nav>
+    <nuxt/>
+  </div>
+</template>
+
+<script>
+import TopNav from "./partials/TopNav";
+export default {
+  components: {
+    TopNav
+  }
+};
+</script>
---
+++ client/layouts/partials/TopNav.vue
@@ -0,0 +1,36 @@
+<template>
+  <!-- START NAV -->
+  <nav class="navbar is-white">
+    <div class="container">
+      <div class="navbar-brand">
+        <a class="navbar-item brand-text" href="../">
+          Admin
+        </a>
+        <div class="navbar-burger burger" data-target="navMenu">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+      <div id="navMenu" class="navbar-menu">
+        <div class="navbar-end">
+          <a class="navbar-item" href="">
+            Sign In
+          </a>
+          <a class="navbar-item" href="">
+            Sign Up
+          </a>
+        </div>
+
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+
+</style>
